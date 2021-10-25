e31449cf5aadba539437b934f430beeeb028fa72
Amanda Steinhebel <asteinhe@gs66-maroon.ndc.nasa.gov>
2021-10-25 13:24:14 -0400

    remove unnecessary arguments, allow user-entered fit ranges

--- enResFit.py
+++ enResFit.py
@@ -9,30 +9,29 @@ import matplotlib.pyplot as plt
 def Gauss(x, A, mu, sigma):
     return A*np.exp(-(x-mu)**2/(2.0*sigma**2))
 
-def SetArray(xmax):
-    #AMANDA - optimize x axis range and bin number (so not hardcoded to 1 and 1000)
-    bins = np.linspace(0,1,500) # Here, the number of bins has been set to 100 and the length of the x axis has been set to one. This can be adjusted; inspect the matplotlib histogram to see what the binning / x axis should be.
-    fit_hist, bins_1 = np.histogram(peaks, bins=bins)
-    bins_2 = np.array([bins[i] for i in range(len(bins)-1)])
-    #bins_2 = 0.5*(bins[0:-1]+bins[1:])
 
-def GaussFit(data, savePlots, bins=500, xmaxin=1, lowFit=0, highFit=1):
+
+def GaussFit(data, savePlots, bins=100, lowFit=0, highFit=np.inf):
     _, xbins0 = np.histogram(data)
     xmax=xbins0.max()
-    histRange=np.linspace(0,xmaxin,bins)
+    histRange=np.linspace(0,xmax,bins)
     ydata,xbins1=np.histogram(data,bins=histRange)
     xbins=xbins1[:-1]
-    #AMANDA - WIP user defined fit window
-    #fitRange=np.linspace(lowFit,highFit,1000)
     xspace = np.linspace(0, xmax, 10000) # This creates a smoother plot when plotting the fit
+    #AMANDA - Instead of defining number of bins, keep bins defined by scope resolution
 
-    # Guesses for p01: [Amplitude, Mu, Sigma]. These guesses must be reasonable.
-    mean=np.mean(ydata)
-    p01 = [ydata.max(), mean, mean/2.]
+    # Guesses for p01: [Amplitude, Mu, Sigma]
+    muGuess = xbins[ydata.argmax()]
+    if (highFit<np.inf): 
+        muGuess = (highFit-lowFit)/2 + lowFit #midpoint of range
+    elif (lowFit>0):
+        muGuess = (xmax-lowFit)/2 + lowFit #can't algebra with infinity
+    p01 = [ydata.max(), muGuess, muGuess/2.]
 
     #AMANDA - include check that fit converges
+    #AMANDA - print chi2/pval from fit
     # popt returns the best fit values for amplitude, mean, and sigma. pcov returns a covariance matrix; the diagonal of this matrix returns the errors associated with the three returned values, which is used to determine the error in the energy resolution.
-    popt, pcov = curve_fit(Gauss, xdata=xbins, ydata=ydata, p0=p01, maxfev=5000)
+    popt, pcov = curve_fit(Gauss, xdata=xbins, ydata=ydata, p0=p01, maxfev=5000, bounds=([0, lowFit, 0], [np.inf, highFit, np.inf]))
 
     plt.figure(figsize = (8, 6))
 
