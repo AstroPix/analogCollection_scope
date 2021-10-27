ce360ab7ddf4acedf6beba6221d38fadf6c2cede
Amanda Steinhebel <asteinhebel151@gmail.com>
2021-10-27 10:17:31 -0400

    Add ability to compare more than only two traces - all add option to turn off ratio plotting and to change the scale when displaying noise STD

--- analysis/darkSensitivity.py
+++ analysis/darkSensitivity.py
@@ -25,7 +25,7 @@ def get_average_trace( filename, dataset ):
 	
 	return mean
 
-def get_baseline_plt(filename, dataset):
+def get_baseline_plt(filename, dataset, xrange=0.004):
 
 	f = h5py.File(filename, 'r')
 	baseline = f[dataset]
@@ -33,7 +33,7 @@ def get_baseline_plt(filename, dataset):
 	rmsArr=[np.std(trace) for trace in baseline] 
 	
 	
-	xspace=np.linspace(0,0.004,50)
+	xspace=np.linspace(0,xrange,50)
 	hist,binEdges=np.histogram(rmsArr, bins=xspace)
 	binWidth=binEdges[1]-binEdges[0]
 	binCenters=binEdges+binWidth
@@ -67,12 +67,16 @@ def plotTraces():
 
 		
 #Compare two traces by plotting together, plotting ratio, and calculating STD of noise from each and plotting
-def plotTraces_compare(fileName, dataset,labels):
+def plotTraces_compare(fileName, dataset,labels, xrange=0.004, ratioBool=True):
 	for pixel in [1, 2]:
 		traces=[]
 		ratio=None
 		noiseRMS=[]
-		fig, axs = plt.subplots(2, sharex=True, figsize=(9,4), gridspec_kw={'height_ratios': [2,1]})
+		if ratioBool:
+			fig, axs = plt.subplots(2, sharex=True, figsize=(9,4), gridspec_kw={'height_ratios': [2,1]})
+		else:
+			#cheat so that ratio plot is not displayed in what is technically a second subfigure
+			fig, axs = plt.subplots(2, sharex=True, figsize=(9,4), gridspec_kw={'height_ratios': [100,1]})
 		fig.suptitle(f"0.05V Injection, pixel {pixel}")
 		for name in fileName:
 			for set in dataset:
@@ -80,23 +84,28 @@ def plotTraces_compare(fileName, dataset,labels):
 				print(file)
 				time=get_time(file,'run1')
 				trace=get_average_trace(file, set)
-				binCenters, hist= get_baseline_plt(file, set)
+				binCenters, hist= get_baseline_plt(file, set, xrange)
 				noiseRMS.append(binCenters)
 				noiseRMS.append(hist)
 				axs[0].plot(time*1e6, trace, label=f"Amp{pixel} {name}")
 				traces.append(trace)
 		ratio=traces[0]/traces[1]
-		axs[1].plot(time*1e6,ratio)
 		axs[0].legend(loc="best")
 		plt.xlabel( "time ($\mu$s)" )
 		plt.setp(axs[0], ylabel="trace - baseline [V]")
-		plt.setp(axs[1],ylabel=f"Ratio {labels[1]}/{labels[2]}")
+		if ratioBool:
+			axs[1].plot(time*1e6,ratio)
+			plt.setp(axs[1],ylabel=f"Ratio {labels[1]}/{labels[2]}")
 		#plt.show()
 		plt.savefig(f"{homeDir}/102221_amp{pixel}_0.05Vinj_traces_compare{labels[0]}.pdf")
 		plt.clf()
 	
-		plt.plot(noiseRMS[0],noiseRMS[1],label=labels[1])
-		plt.plot(noiseRMS[2],noiseRMS[3],label=labels[2])
+		labelIndex=1
+		for i in range(len(noiseRMS)): 
+			if i%2!=0: #Find end of plotting pair
+				plt.plot(noiseRMS[i-1],noiseRMS[i],label=labels[labelIndex])
+				labelIndex+=1
+		#plt.plot(noiseRMS[2],noiseRMS[3],label=labels[2])
 		plt.title(f"Noise RMS pixel{pixel}")
 		plt.xlabel("RMS [V]")
 		plt.ylabel("counts")
@@ -104,9 +113,7 @@ def plotTraces_compare(fileName, dataset,labels):
 		#plt.show()
 		plt.savefig(f"{homeDir}/102221_amp{pixel}_0.05Vinj_noiseRMS_{labels[0]}.pdf")
 		plt.clf()
-		
-		
-		
+			
 		
 		
 		
@@ -116,13 +123,19 @@ def plotTraces_compare(fileName, dataset,labels):
 		
 if __name__ == "__main__":
 	fileName=["","_dark"]
+	trigScanVal=[5,10,20,50,100,200,500,1000]
+	fileName_scan=["_trigScan_"+str(i)+"mV" for i in trigScanVal]
 	dataset=["run1","run2"]
 	dark=["","Light","Dark"]
 	gain=["Gain","gain1","gain2"]
+	trigScan=[str(i)+"mV" for i in trigScanVal]
+	trigScan.insert(0,"scaleScan")
 	homeDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp"
+	
 	plotTraces()		
 	plotTraces_compare([""],dataset,gain)	
 	plotTraces_compare(fileName,["run1"],dark)	
+	plotTraces_compare(fileName_scan,["run1"],trigScan,xrange=0.03,ratioBool=False)	
 		
 		
 		
