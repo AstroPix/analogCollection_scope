df8ae4e9fd37411f21508216bb9c2db3cc4bc9e0
Amanda Steinhebel <asteinhebel151@gmail.com>
2021-11-02 15:09:23 -0400

    Add function to fit compton edge

--- analysis/enResFitting.py
+++ analysis/enResFitting.py
@@ -3,6 +3,7 @@ import numpy as np
 import h5py
 import scipy
 from scipy.optimize import curve_fit
+from scipy import special
 
 #################
 #
@@ -10,12 +11,15 @@ from scipy.optimize import curve_fit
 #
 ###################
 
-# Define the fit function (a Gaussian)
 def Gauss(x, A, mu, sigma):
     return A*np.exp(-(x-mu)**2/(2.0*sigma**2))
+    
+def Edge(x,A,B,mu,sigma):
+	return B*special.erfc((x-mu)/(np.sqrt(2)*sigma))+A
+	#https://arxiv.org/pdf/1610.09185.pdf
+    
 
-def getArrayIndex(array, low,high):
-	
+def getArrayIndex(array, low,high):	
 	goodValues=[]
 	for i in range(len(array)):
 		if array[i]>low and array[i]<high:
@@ -137,8 +141,6 @@ def getMin(file, integral=0, dataset='run1'):
 	return np.min(data)
 	
 	
-	
-
 #Calculate error on fit parameters, save in text file	
 def printParams(file, popt, en_res, pcov, savePlots,integral=False):
 	#Distinguish between peaks and integral
@@ -173,3 +175,168 @@ def printParams(file, popt, en_res, pcov, savePlots,integral=False):
 		print("Energy resolution = %0.2f" %(abs(en_res))+"%\n")
 		print("Error in amplitude = %0.3f \nError in mu = %0.6f \nError in sigma = %0.6f" %(Amp_err, Mu_err, Sigma_err)+"\n")
 		print("Error in energy resolution = %0.5f"%(stdev_er)+"%\n")
+
+
+#Use linear fit coefficients to scale data to keV before plot/fit
+def enResPlot_linearScale(settings, coef, integral=0, fitLow=0, fitHigh=np.inf, dataset='run1'):
+	#Define inputs
+	file=settings[0]
+	title=settings[1]
+	pixel=settings[2]
+	savePlots=settings[3]
+
+	#Distinguish between peaks and integral
+	if (integral>0):
+		datain='_integral'
+	else: #peaks
+		datain='_peaks'
+
+	#get info from file
+	print(file)
+	f = h5py.File(file,'r')
+	dsName=dataset
+	data=f[dsName+datain]
+	data=[(coef[0]*x+coef[1]) for x in data]
+	
+	
+	#Create arrays for binning based on scope resolution
+	xBinWidth=1.0 #1.0keV bins
+	xMax=np.max(data)
+	#give negative space to see full distribution
+	if integral>0:
+		xMin=np.min(data)-10
+	else:
+		xMin=-10
+	binEdges=np.arange(xMin,xMax+xBinWidth,xBinWidth)#use peakMax+xBinWidth to overshoot range and include all data
+	
+	#Create histogram of data
+	hist=plt.hist(data,bins=binEdges,label=r'Data', color='blue')
+	ydata=hist[0]
+	binCenters=hist[1]+xBinWidth/2
+	binCenters=binCenters[:-1]
+	
+	#Set fit range
+	low_i,high_i=getArrayIndex(binCenters,fitLow,fitHigh)
+	
+	#Set up fit with guesses for p01: [Amplitude, Mu, Sigma]
+	muGuess=np.mean(data)
+	if muGuess>fitHigh or muGuess<fitLow:
+		muGuess=binCenters[low_i]+(binCenters[high_i]-binCenters[low_i])/2.
+	ampGuess=ydata[low_i:high_i].max()
+	p01 = [ampGuess, muGuess, muGuess/2.]
+	
+	#Fit histogram over desired range
+	popt, pcov = curve_fit(Gauss, xdata=binCenters[low_i:high_i], ydata=ydata[low_i:high_i], p0=p01, bounds=(0,np.inf), maxfev=5000)
+	(Amp,Mu,Sigma)=popt
+	#range is set with low_i and high_i index values for input arrays
+	#bounds keeps all parameters positive
+	
+	#Calculate energy resolution
+	enRes = (2.355*abs(Sigma)*100)/Mu # Calculates energy resolution, 2.355 converts sigma to FWHM
+	
+	#Display fit on final histogram
+	plt.rc('text', usetex=True) #use Latex
+	xspace=np.linspace(xMin,binEdges[-1],len(binCenters)*10)
+	plt.plot(xspace, Gauss(xspace, *popt), 'r-', label=r'Fit')    
+	plt.plot([], [], ' ', label=f"Energy res = {enRes:.2f}%")
+	plt.plot([], [], ' ', label=r"Fit parameters:")
+	plt.plot([], [], ' ', label=f"\small Amp={Amp:.2f}, $\mu$={Mu:.3f}, $\sigma$={Sigma:.3f}")
+	plt.legend(loc="best")
+	plt.xlabel('Calibrated Energy [keV]')
+	plt.ylabel('Counts')
+	plt.title(f"Energy Resolution - {title}, pixel {pixel}")
+		
+	
+	#save figure
+	saveto=file[:-5]
+	saveto=f"{saveto}{datain}EnRes{enRes:.2f}.pdf"
+	plt.savefig(saveto) if savePlots else plt.show()
+	plt.clf()
+	
+	f.close()
+
+	return popt, enRes, pcov
+	
+	
+#Plot data, fit to Gaussian, calculate energy resolution and draw plot
+def enResPlot_edge(settings, integral=0, fitLow=0, fitHigh=np.inf, dataset='run1'):
+	#Define inputs
+	file=settings[0]
+	title=settings[1]
+	pixel=settings[2]
+	savePlots=settings[3]
+
+	#Distinguish between peaks and integral
+	if (integral>0):
+		datain='_integral'
+	else: #peaks
+		datain='_peaks'
+
+	#get info from file
+	print(file)
+	f = h5py.File(file,'r')
+	dsName=dataset
+	data=f[dsName+datain]
+	scaling=f[dsName+'_scaling']
+	
+	#Create arrays for binning based on scope resolution
+	xBinWidth=scaling[3]#YMULT Value
+	xMax=np.max(data)
+	if integral>0:
+		xMin=np.min(data)
+	else:
+		xMin=0
+	if integral>0:
+		xBinWidth*=integral
+	binEdges=np.arange(xMin,xMax+xBinWidth,xBinWidth)#use peakMax+xBinWidth to overshoot range and include all data
+	
+	#Create histogram of data
+	hist=plt.hist(data,bins=binEdges,label=r'Data', color='blue')
+	ydata=hist[0]
+	binCenters=hist[1]+xBinWidth/2
+	binCenters=binCenters[:-1]
+	
+	#Set fit range
+	low_i,high_i=getArrayIndex(binCenters,fitLow,fitHigh)
+	
+	#Set up fit with guesses for p01: [Amplitude, Mu, Sigma]
+	muGuess=np.mean(data)
+	if muGuess>fitHigh or muGuess<fitLow:
+		muGuess=binCenters[low_i]+(binCenters[high_i]-binCenters[low_i])/2.
+	ampGuess=ydata[low_i:high_i].max()
+	p01 = [ampGuess, ampGuess, muGuess, muGuess/2.]
+	
+	#Fit histogram over desired range
+	popt, pcov = curve_fit(Edge, xdata=binCenters[low_i:high_i], ydata=ydata[low_i:high_i], p0=p01, bounds=(0,np.inf), maxfev=5000)
+	(Amp1, Amp2, Mu, Sigma)=popt
+	#range is set with low_i and high_i index values for input arrays
+	#bounds keeps all parameters positive
+	
+	#Calculate energy resolution
+	enRes = (2.355*abs(Sigma)*100)/Mu # Calculates energy resolution, 2.355 converts sigma to FWHM
+	
+	#Display fit on final histogram
+	plt.rc('text', usetex=True) #use Latex
+	xspace=np.linspace(xMin,binEdges[-1],len(binCenters)*10)
+	plt.plot(xspace, Edge(xspace, *popt), 'r-', label=r'Fit')  
+	plt.plot([], [], ' ', label=f"Energy res = {enRes:.2f}%")  
+	plt.plot([], [], ' ', label=r"Fit parameters:")
+	plt.plot([], [], ' ', label=f"\small Amp1={Amp1:.2f}, Amp2={Amp2:.2f}, $\mu$={Mu:.3f}, $\sigma$={Sigma:.3f}")
+	plt.legend(loc="best")
+	if integral:
+		plt.xlabel('Integrated energy [V]')
+	else:
+		plt.xlabel('Peak Energy [V]')
+	plt.ylabel('Counts')
+	plt.title(f"Edge Fit - {title}, pixel {pixel}")
+		
+	
+	#save figure
+	saveto=file[:-5]
+	saveto=f"{saveto}{datain}EdgeFit_EnRes{enRes:.2f}.pdf"
+	plt.savefig(saveto) if savePlots else plt.show()
+	plt.clf()
+	
+	f.close()
+
+	return popt, enRes, pcov
---
+++ analysis/energyCalibration.py
@@ -0,0 +1,135 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import h5py
+import scipy
+from scipy.optimize import curve_fit
+from scipy import stats
+import sys,os
+import enResFitting
+
+
+savePlots=False
+
+
+
+def energyCalibFit(trueEn, data, dataName,saveto):
+	
+	dataNameStr=dataName.replace(" ", "")
+	
+	amp1_p=[]
+	amp1_i=[]
+	
+	for photopeak in data:
+		flatData=np.array(photopeak).flatten('F')
+		amp1_p.append(flatData[0])
+		amp1_i.append(flatData[1])
+		
+	coef = np.polyfit(amp1_p,trueEn,1)
+	poly1d_fn = np.poly1d(coef) 
+	plt.scatter(amp1_p,trueEn,label='Amp1',marker="o")
+	plt.plot(amp1_p, poly1d_fn(amp1_p), '--k',label=f"y={coef[0]:.3f}x{coef[1]:.3f}")
+	print(f"Peak linear fit: y={coef[0]:.3f}x{coef[1]:.3f}")
+	plt.xlabel(f"{dataName} (from peak height)")
+	plt.ylabel("True Energy [keV]")
+	plt.legend(loc="best")
+	#plt.yscale('log')
+	#plt.show()
+	#plt.savefig(f"{saveto}_peaks_{dataNameStr}_log.pdf")
+	plt.savefig(f"{saveto}_peaks_{dataNameStr}.pdf") if savePlots else plt.show()
+	plt.clf()
+	
+	coef2 = np.polyfit(amp1_i,trueEn,1)
+	poly1d_fn2 = np.poly1d(coef2) 	
+	plt.scatter(amp1_i,trueEn,label='Amp1',marker="o")
+	plt.plot(amp1_i, poly1d_fn2(amp1_i), '--k', label=f"y={coef2[0]:.3f}x+{coef2[1]:.3f}")
+	print(f"Integral linear fit: y={coef2[0]:.3f}x+{coef2[1]:.3f}")
+	plt.xlabel(f"{dataName} (from integral)")
+	plt.ylabel("True Energy [keV]")
+	plt.legend(loc="best")
+	#plt.yscale('log')
+	plt.legend(loc="best")
+	#plt.show()
+	#plt.savefig(f"{saveto}_integral_{dataNameStr}_log.pdf")
+	plt.savefig(f"{saveto}_integral_{dataNameStr}.pdf") if savePlots else plt.show()
+	plt.clf()
+	
+	return coef,coef2
+	
+###########################################################################
+#injection=[i*0.1 for i in range(1,19)]injection=[i*0.1 for i in range(1,3)]
+
+
+enResArr=[]
+muArr=[]
+homeDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/"
+
+fileList=["102021_amp1/cadmium109_45min.h5py", "102821_amp1/cadmium109_16h.h5py", "102021_amp1/cobalt57_14h.h5py"]
+energyList=[22.16, 88.03, 122.06]
+nameList=["Cadmium109", "Cadmium109", "Cobalt57"]
+
+fitLow_p=[0.05,0.25,0.29]
+fitLow_i=[150,725,1700]
+
+
+"""
+i=0
+enRes_tmp, mu_tmp=[], []
+for file in fileList:
+	for pixel in [1]:
+		settings=[homeDir+file, nameList[i], pixel, savePlots]
+		popt, enRes, pcov = enResFitting.enResPlot(settings,fitLow=fitLow_p[i])
+		enResFitting.printParams(file, popt, enRes, pcov, savePlots)
+		poptI, enResI, pcovI = enResFitting.enResPlot(settings, fitLow=fitLow_i[i], integral=10000)
+		enResFitting.printParams(file, poptI, enResI, pcovI, savePlots, integral=10000)
+		enRes_tmp.append([enRes, enResI])
+		mu_tmp.append([popt[1], poptI[1]])
+	enResArr.append(enRes_tmp)
+	muArr.append(mu_tmp)
+	enRes_tmp,mu_tmp=[],[]
+	i+=1
+		
+coef_p,coef_i=energyCalibFit(energyList, muArr, "Fit Mean [V]",homeDir)	
+
+"""
+
+
+#trigScan fit: y=0.001x-0.6 (y=measured, x=trigThreshold)
+#for Am, trig = 550 mV -> -0.05 measured
+
+
+#zero point from baseline pulled from scope
+muArr=[[-0.05,-0.05],[0.0705,189.6605],[0.2597,742.7046],[0.3049,1812.8481]]
+
+energyList.insert(0,0)#zero energy point
+file="102921_amp1/americium241_90min.h5py"
+settings=[homeDir+file, "Americium241", 1, savePlots]
+popt, enRes, pcov = enResFitting.enResPlot_linearScale(settings,coef_p,fitLow=50)
+enResFitting.printParams(file, popt, enRes, pcov, savePlots)
+	
+
+file="102021_amp1/cadmium109_45min.h5py"
+settings=[homeDir+file, "Cadmium109", 1, savePlots]
+popt, enRes, pcov = enResFitting.enResPlot_linearScale(settings,coef_p)
+enResFitting.printParams(file, popt, enRes, pcov, savePlots)
+
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
