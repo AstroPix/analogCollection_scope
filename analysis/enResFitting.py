fix error calculation method so no division by zero

analysis/enResFitting.py
@@ -454,8 +454,9 @@ def calcError(sigmaArr, nArr):
 	errArr=[]
 	#error Arr1ay = sigma/sqrt(N) (for edge, 2sig integral from mu)
 	for j in range(len(sigmaArr)):
-		err_p=sigmaArr[j][0]/np.sqrt(nArr[j][0])
-		err_i=sigmaArr[j][1]/np.sqrt(nArr[j][1])
+		err_p, err_i = 0,0
+		err_p == -1 if nArr[j][0]<=0 else err_p==sigmaArr[j][0]/np.sqrt(nArr[j][0])
+		err_i == -1 if nArr[j][1]<=0 else err_i==sigmaArr[j][1]/np.sqrt(nArr[j][1])
 		errArr.append([err_p,err_i])
 		
 	return errArr