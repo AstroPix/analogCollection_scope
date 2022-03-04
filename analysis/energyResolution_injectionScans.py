c960631bca2f830d19cd75ce7e9fc4f80bea2f69
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-03-04 11:38:54 -0500

    update hardcoded file locations for v2 - TODO make v1/v2 run options to recreate plots

--- analysis/energyResolution_injectionScans.py
+++ analysis/energyResolution_injectionScans.py
@@ -19,14 +19,15 @@ def injScanPlot(inj, data, err, dataName, saveto, chip, fit=False):
 	dataNameStr=dataName.replace(" ", "")
 	
 	amp1_p=data[0]
-	amp2_p=data[1]
+	#amp2_p=data[1]
 	
 	err1=err[0]
-	err2=err[1]
+	#err2=err[1]
 
-	plt.errorbar(inj,amp1_p,yerr=err1,label='Amp1',marker="o",linestyle='None')
-	plt.errorbar(inj,amp2_p,yerr=err2,label='Amp2',marker="o",linestyle='None')
-	plt.title(f"Chip00{chip}")
+	plt.errorbar(inj,amp1_p,yerr=err1,label='Amp1',marker="o",linestyle='None',color='r')
+	#plt.errorbar(inj,amp2_p,yerr=err2,label='Amp2',marker="o",linestyle='None')
+	#plt.title(f"Chip00{chip}")
+	plt.title("v2")
 	plt.xlabel("Injection voltage [V]")
 	plt.legend(loc="best")
 	if traceInteg:
@@ -41,19 +42,21 @@ def injScanPlot(inj, data, err, dataName, saveto, chip, fit=False):
 		x = np.linspace(inj[1],inj[-3], 180)
 		coef1i = np.polyfit(inj[1:-3],amp1_p[1:-3],1)
 		poly1d_fn1i = np.poly1d(coef1i) 
-		coef2i = np.polyfit(inj[1:-3],amp2_p[1:-3],1)
-		poly1d_fn2i = np.poly1d(coef2i) 
+		#coef2i = np.polyfit(inj[1:-3],amp2_p[1:-3],1)
+		#poly1d_fn2i = np.poly1d(coef2i) 
 		plt.plot(x, poly1d_fn1i(x), '--b',label=f"y={coef1i[0]:.3f}x+{coef1i[1]:.3f}")
-		plt.plot(x, poly1d_fn2i(x), '--',color='orange',label=f"y={coef2i[0]:.3f}x+{coef2i[1]:.3f}")
+		#plt.plot(x, poly1d_fn2i(x), '--',color='orange',label=f"y={coef2i[0]:.3f}x+{coef2i[1]:.3f}")
 
 
 	
 ###########################################################################
 #Lowest injection values (0.1 - 0.3V) have scope scalings that are too large relative to the distribution
-injection=[i*0.1 for i in range(5,14)] #1,19
+injection=[i*0.1 for i in range(3,16)] #1,19
 homeDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp"
-saveDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/chip003/"
-runList = [[102221,1],[102221,2]] #day of run, pixel number
+saveDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/"
+#runList = [[102221,1],[102221,2]] #day of run, pixel number #v1
+runList=[['030122',1]]
+binsize=0.01 if traceInteg else 0.001
 
 enResArr, muArr, sigmaArr, muErrArr, sigErrArr = [],[],[],[],[]
 
@@ -62,9 +65,10 @@ chip=3
 for i,element in enumerate(runList):
 	enResArr_tmp, muArr_tmp, sigmaArr_tmp, nArr_tmp, muErrArr_tmp, sigErrArr_tmp = [],[],[],[],[],[]
 	for inj in injection:
-		file=f"{homeDir}/{element[0]}_amp{element[1]}/{inj:.1f}Vinj.h5py"
+		#file=f"{homeDir}/{element[0]}_amp{element[1]}/{inj:.1f}Vinj.h5py"
+		file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/scan_{inj:.1f}Vinj_2min.h5py"
 		settings=[file, f"{inj:.1f}V-injection", element[1], inj, savePlots, chip]
-		popt, enRes, pcov = enResFitting.enResPlot(settings, savedir=saveDir, integral=traceInteg, injection=True)
+		popt, enRes, pcov = enResFitting.enResPlot(settings, savedir=saveDir, integral=traceInteg, injection=True, binSize=binsize)
 		enResFitting.printParams(settings, popt, enRes, pcov, savedir=saveDir, integral=traceInteg, injection=True)
 		enResArr_tmp.append(enRes)
 		muArr_tmp.append(popt[1])
@@ -80,7 +84,7 @@ for i,element in enumerate(runList):
 injScanPlot(injection, enResArr, np.zeros(len(enResArr)), "Energy Resolution [\%]", saveDir, chip)#no easily returned value for enResErr
 injScanPlot(injection, muArr, muErrArr, "Fit Mean [V]",saveDir,chip,fit=True)		
 		
-		
+"""		
 #chip4
 saveDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/chip004/"
 enResArr, muArr, sigmaArr, muErrArr, sigErrArr = [],[],[],[],[]
@@ -107,5 +111,5 @@ for i,element in enumerate(runList):
 #plot two pixels in each chip against each other				
 injScanPlot(injection, enResArr, np.zeros(len(enResArr)), "Energy Resolution [\%]", saveDir, chip)#no easily returned value for enResErr
 injScanPlot(injection, muArr, muErrArr, "Fit Mean [V]",saveDir,chip,fit=True)			
-		
+"""		
 		
