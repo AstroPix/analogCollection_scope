23dec7f66980be5b7455b650fa455c70bfa43957
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-04-12 11:45:52 -0400

    Hardcode fitting windows for v2 chip2

--- analysis/energyCalibration/energyCalibration.py
+++ analysis/energyCalibration/energyCalibration.py
@@ -26,10 +26,14 @@ for key,val in inputDict.items():
 savePlots=bool(int(savePlots))
 fitSpectra=bool(int(fitSpectra))
 traceInteg=bool(int(traceInteg))
+vers=int(version)
 pix=int(pix)
 fit=int(fit)
 chip=int(chip)
 
+#AMANDA UPDATE
+#if using v2, use same noise cut as v1 chip3 pix 1 (20 mV)
+#eliminate v2 and amp2 option
 
 ########################################################################################
 ###########################################################################
@@ -75,11 +79,12 @@ def fixFileList(fileList):
 
 def getFiles(amp):
 	os.chdir(sys.path[0])
-	if amp==1:
+	if amp==1 and vers==1:
 		with open(input1, 'r') as files1:
 			fileList=files1.readlines()
 		if chip==3:
 			energyList=[22.16, 88.03, 14.41, 122.06, 59.54, 30.97]
+			binsize=np.zeros(len(energyList))
 			nameList=["Cadmium109", "Cadmium109", "Cobalt57", "Cobalt57", "Americium241", "Barium133"]
 			#fine-tune the range so that individual peaks are picked out
 			if not traceInteg:
@@ -90,6 +95,7 @@ def getFiles(amp):
 				fitHighArr=[30,80,100,80,60,100]
 		elif chip==4:
 			energyList=[22.16, 88.03, 14.41, 122.06]
+			binsize=np.zeros(len(energyList))
 			nameList=["Cadmium109", "Cadmium109", "Cobalt57", "Cobalt57"]
 			if not traceInteg:
 				fitLowArr=[0.0,0.244,0.0,0.29]
@@ -97,11 +103,27 @@ def getFiles(amp):
 			else:
 				fitLowArr=[0,50,0,80]
 				fitHighArr=[30,80,100,120]
+	elif amp==1 and vers==2:
+		with open(input1, 'r') as files1:
+			fileList=files1.readlines()
+		if chip==2:
+			energyList=[22.16, 88.03,122.06,14.41,30.1]
+			nameList=["Cadmium109", "Cadmium109", "Cobalt57", "Cobalt57","Barium133"]
+			#fine-tune the range so that individual peaks are picked out
+			if not traceInteg:
+				fitLowArr=[0.0,0.44,0.48,0,0]
+				fitHighArr=[0.25,0.6,0.55,0.15,0.3]
+				binsize=[0.01,0.007,0.005,0.005,0.005]
+			else:
+				fitLowArr=[0,70,80,0,0]
+				fitHighArr=[30,80,120,120,100]
+				binsize=np.zeros(len(energyList))
 	elif amp==2:
 		with open(input2, 'r') as files2:
 			fileList=files2.readlines()
 		if chip==3:
 			energyList=[14.41, 122.06, 59.54, 22.16, 88.03, 30.97]
+			binsize=np.zeros(len(energyList))
 			nameList=["Cobalt57","Cobalt57", "Americium241", "Cadmium109", "Cadmium109","Barium133"]
 			#fine-tune the range so that individual peaks are picked out
 			if not traceInteg:
@@ -112,6 +134,7 @@ def getFiles(amp):
 				fitHighArr=[20,60,30,15,43,20]
 		elif chip==4:
 			energyList=[22.16, 88.03, 14.41, 122.06]
+			binsize=np.zeros(len(energyList))
 			nameList=["Cadmium109", "Cadmium109", "Cobalt57", "Cobalt57"]
 			if not traceInteg:	
 				fitLowArr=[0.02,0.18,0.0,0.24]
@@ -124,7 +147,7 @@ def getFiles(amp):
 		fileList,energyList,nameList,fitLowArr, fitHighArr = [],[],[],[],[]
 
 	fileList=fixFileList(fileList)
-	return fileList, energyList, nameList, fitLowArr, fitHighArr
+	return fileList, energyList, nameList, fitLowArr, fitHighArr, binsize
 
 def getEdgeFiles(amp):
 	os.chdir(sys.path[0])
@@ -206,7 +229,7 @@ def energyCalibFit(trueEn, data, err, dataName, saveto, integral):
 		coef3, coef3_pcov = curve_fit(triFit,trueEn,data,sigma=err,absolute_sigma=True) #3rd deg poly
 		triPlot=triFit(x,*coef3)
 		plt.plot(x, triPlot, '--b',label=f"3rd deg. polynomial")
-		print(f"Peak 3rd deg poly fit: y={coef3[0]:.3f}x$^3$+{coef3[1]:.3f}x$^2$+{coef3[2]:.3f}x + {coef3[3]:.3f}")
+		print(f"Peak 3rd deg poly fit: y={coef3[0]:.10f}x$^3$+{coef3[1]:.5f}x$^2$+{coef3[2]:.5f}x + {coef3[3]:.5f}")
 		#invert - if value outside of calibrated range, assign it a value of -1 (underflow)
 		invertedFn=interpolate.interp1d(triPlot,x,fill_value="extrapolate")
 		sum_square=getSumSq(trueEn_sorted, data_sorted, err_sorted, invertedFn)
@@ -309,13 +332,13 @@ enResArr, muArr, sigmaArr, nArr, muErrArr, sigErrArr = [],[],[],[],[],[]
 
 
 if fitSpectra:
-	fileList,energyList,nameList,fitLowArr,fitHighArr = getFiles(pix)
+	fileList,energyList,nameList,fitLowArr,fitHighArr,binsize = getFiles(pix)
 
 	#loop through all files, fit with Gaussian, return mean/sigma/energy resolution and store in arrays - separately for each amp
 	i=0
 	for file in fileList:
 		settings=[file, nameList[i], pix, energyList[i], savePlots, chip]
-		popt, enRes, pcov = enResFitting.enResPlot(settings,fitLow=fitLowArr[i], fitHigh=fitHighArr[i], savedir=dataDir, integral=traceInteg)
+		popt, enRes, pcov = enResFitting.enResPlot(settings,fitLow=fitLowArr[i], fitHigh=fitHighArr[i], savedir=dataDir, integral=traceInteg, binSize=binsize[i])
 		enResFitting.printParams(settings, popt, enRes, pcov, savedir=dataDir, integral=traceInteg)
 		enResArr.append(enRes)
 		muArr.append(popt[1])
@@ -351,15 +374,32 @@ fitFn = energyCalibFit(energyList, muArr, muErrArr, "Fit Mean [V]", saveDir, tra
 
 #use calibration curve to calibrate a spectrum
 if pix==1:
+	if chip==2:
+	#chip002 v2#not fitting edge
+		files=["030322_amp1/TEST_chip2_cadmium109_10min.h5py","030822_amp1/chip2_200mV_cadmium109_330min_combined.h5py","030422_amp1/chip2_150mV_cobalt57_150min.h5py","030422_amp1/chip2_75mV_cobalt57_120min.h5py","030722_amp1/chip2_100mV_barium133_960min.h5py"]
+		name=["Cadmium109-calib","Cadmium109-calib","Cobalt57-calib","Cobalt57-calib","Barium133-calib"]
+		trueEn=[22.16,88.03,122.06,14.41,30.1]
+		binSizeArr=[0.5,1.2,2.5,0.5,1]#default 0
+		if traceInteg:
+			fitLowArr=[0,0,100,0,0]
+			fitHighArr=[np.inf,np.inf,np.inf,np.inf,np.inf]
+		else:
+			fitLowArr=[0,84,100,0,0] #default 0
+			fitHighArr=[40,np.inf,np.inf,np.inf,np.inf] #default np.inf
+			
+		for i,f in enumerate(files):
+			settings=[homeDir+f,name[i],pix,trueEn[i],savePlots,chip]
+			popt, enRes, pcov = enResFitting.enResPlot(settings,fit=fit,coef=fitFn,fitLow=fitLowArr[i], fitHigh=fitHighArr[i], binSize=binSizeArr[i], integral=traceInteg)
+			enResFitting.printParams(settings, popt, enRes, pcov, integral=traceInteg)
+	
 	if chip==3:
-	#chip003
+	#chip003 v1
 		#not fitting edge
 		files=["110421_amp1/Americium_480min_combined.h5py","102021_amp1/cadmium109_45min.h5py","102821_amp1/cadmium109_16h.h5py","110821_amp1/barium133_combined_65min.h5py","102021_amp1/cobalt57_14h.h5py","102021_amp1/cobalt57_14h.h5py"]
 		name=["Americium241-calib","Cadmium109-calib","Cadmium109-calib","Barium133-calib","Cobalt57-calib","Cobalt57-calib"]
 		trueEn=[59.54,22.16,88.03,30.97,122.06,14.41]
 		binSizeArr=[0,0,1,1,1,0,0]#default 0
 		if traceInteg:
-			print("reading the correct values")
 			fitLowArr=[50,0,0,60,100,0]
 			fitHighArr=[np.inf,np.inf,np.inf,np.inf,np.inf,np.inf]
 		else:
@@ -372,7 +412,7 @@ if pix==1:
 			enResFitting.printParams(settings, popt, enRes, pcov, integral=traceInteg)
 	
 	elif chip==4:		
-	#chip004
+	#chip004 v1
 		files=["120921_amp1/15mV_chip004_AC_Cadmium_120min.h5py","120921_amp1/90mV_chip004_AC_Cadmium_1200min.h5py","120721_amp1/lowPeak_chip004_AC_cobalt57_60min.h5py","120721_amp1/highPeak_chip004_AC_cobalt57_960min.h5py"]
 		name=["Cadmium109-calib","Cadmium109-calib","Cobalt57-calib","Cobalt57-calib"]
 		trueEn=[22.16,88.03,14.41,122.06]
@@ -392,7 +432,7 @@ if pix==1:
 					
 elif pix==2:
 	if chip==3:
-	#chip003
+	#chip003 v1
 		files=["111521_amp2/overnight_Americium241_960min.h5py","111621_amp2/day_Cadmium109_300min.h5py","111221_amp2/weekend_Cobalt57_4020min.h5py","111221_amp2/weekend_Cobalt57_4020min.h5py","120221_amp2/calib_barium133_180min.h5py","120221_amp2/calib_cadmium190_1080min.h5py"]
 		name=["Americium241-calib","Cadmium109-calib","Cobalt57-calib","Cobalt57-calib","Barium133-calib", "Cadmium109-calib"]
 		trueEn=[59.54,22.16,122.06,14.41,30.97, 88.03]
@@ -411,7 +451,7 @@ elif pix==2:
 	
 
 	elif chip==4:
-	#chip004
+	#chip004 v1
 		files=["121321_amp2/15mV_chip004_AC_cadmium109_120min.h5py","121321_amp2/90mV_chip004_AC_cadmium109_1200min.h5py","121521_amp2/15mV_chip004_cobalt57_60min.h5py", "011022_amp2/chip004_cobalt57_combined_2220min.h5py"]
 		name=["Cadmium109-calib","Cadmium109-calib","Cobalt57-calib","Cobalt57-calib"]
 		trueEn=[22.16,88.03,14.41, 122.06]
