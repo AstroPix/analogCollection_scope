dd1fb584dbdf6e5c20c6483367453beab0306ee8
Amanda Steinhebel <asteinhebel151@gmail.com>
2022-04-20 10:34:52 -0400

    add option to compare injection scans for v1 and v2

--- analysis/energyResolution_injectionScans.py
+++ analysis/energyResolution_injectionScans.py
@@ -9,11 +9,11 @@ import enResFitting
 
 #######################
 #Global variables
-savePlots=True
+savePlots=False
 #Fit peak heights or integral value (scaled by scope resolution)
 #If traceInteg is true, use integral. If false, use peak height
 traceInteg = True
-v1=False #if v1=False, generate plots for v2
+vers=12 #1 if v1, 2 if v2, 12 if comparing v1 and v2
 
 
 #######################
@@ -27,15 +27,19 @@ def injScanPlot(inj, data, err, dataName, saveto, chip, fit=False):
 	amp2_p=data[1]
 	err2=err[1]
 
-	if v1:
+	if vers==1:
 		plt.title(f"Chip00{chip}")
 		labels=['Amp1','Amp2']
 		chip=f"_chip{chip}"
-	else:
+	elif vers==2:
 		plt.title("v2")
 		#labels=['Chip1','Chip2']
 		labels=['Orig DACs', 'Updated DACs']
 		chip=""
+	elif vers==12:
+		plt.title("v1 vs v2")
+		labels=['v1 (chip003 amp1)','v2 (chip1 amp1)']
+		chip="_compareVers12"
 	plt.errorbar(inj,amp1_p,yerr=err1,label=labels[0],marker="o",linestyle='None',color='r')
 	plt.errorbar(inj,amp2_p,yerr=err2,label=labels[1],marker="o",linestyle='None')
 
@@ -63,20 +67,24 @@ def injScanPlot(inj, data, err, dataName, saveto, chip, fit=False):
 ###########################################################################
 #MAIN
 #Lowest injection values (0.1 - 0.3V) have scope scalings that are too large relative to the distribution
-injection=[i*0.1 for i in range(1,19)] #1,19
+injection=[round(i*0.1,1) for i in range(1,19)] #1,19
 homeDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp"
-if v1:
+if vers==1:
 	saveDir = ["/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v1/chip003/","/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v1/chip003/"]
 	runList = [[102221,1],[102221,2]] #day of run, pixel number #v1
-else:
+elif vers==2:
 	#saveDir = ["/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/chip1/origDACs/","/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/chip2/origDACs/"]
 	#runList=[['030122',1],['030322',1]] #orig DACs settings
 	saveDir = ["/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/chip1/origDACs/","/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/chip1/"]
 	runList=[['030122',1],['032122',1]] #orig vs updated DACs settings, chip1
+elif vers==12:
+	saveDir = ["/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v1/chip003/","/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/chip1/"]
+	runList=[['102221',1],['032122',1]] #orig vs updated DACs settings, chip1
+
 binsize=0.002 
 if traceInteg:
 	binsize=0.05
-elif v1:
+if vers==1 or vers==12: #update v2 binning when file called
 	binsize=0 #default - based off scope resolution
 
 enResArr, muArr, sigmaArr, muErrArr, sigErrArr = [],[],[],[],[]
@@ -87,13 +95,18 @@ chip=3
 for i,element in enumerate(runList):
 	enResArr_tmp, muArr_tmp, sigmaArr_tmp, nArr_tmp, muErrArr_tmp, sigErrArr_tmp = [],[],[],[],[],[]
 	for inj in injection:
-		if v1:
+		if vers==1 or (vers==12 and i==0):
 			file=f"{homeDir}/v1/{element[0]}_amp{element[1]}/{inj:.1f}Vinj.h5py"
+		elif vers==12 and i!=0:
+			if traceInteg:
+				binsize=0.05
+			else:
+				binsize=0.002
+			file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/chip1_{inj:.1f}Vinj_2min.h5py"
 		elif i==0: #v2 chip1
 			file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/scan_{inj:.1f}Vinj_2min.h5py"
 		else: #v2 chip2
-			#file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/scan_chip2_{inj:.1f}Vinj_2min.h5py"
-			file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/chip1_{inj:.1f}Vinj_2min.h5py"
+			file=f"{homeDir}/v2/{element[0]}_amp{element[1]}/scan_chip2_{inj:.1f}Vinj_2min.h5py"
 		settings=[file, f"{inj:.1f}V-injection", element[1], inj, savePlots, chip]
 		popt, enRes, pcov = enResFitting.enResPlot(settings, savedir=saveDir[i], integral=traceInteg, injection=True, binSize=binsize)
 		enResFitting.printParams(settings, popt, enRes, pcov, savedir=saveDir[i], integral=traceInteg, injection=True)
@@ -108,15 +121,17 @@ for i,element in enumerate(runList):
 	muErrArr.append(muErrArr_tmp)
 	sigErrArr.append(sigErrArr_tmp)
 #plot v1 pixels or v2 chips against each other		
-if v1:
+if vers==1:
 	tosave=saveDir[0]
-else:
-	#tosave="/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/origDACs"
+elif vers==2:
 	tosave="/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v2/"
+else:
+	tosave="/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/"
 injScanPlot(injection, enResArr, np.zeros(len(enResArr)), "Energy Resolution [\%]", tosave, chip)#no easily returned value for enResErr
 injScanPlot(injection, muArr, muErrArr, "Fit Mean [V]",tosave,chip,fit=True)		
-		
-if v1:	
+	
+#continue to consider v1 chip4	
+if vers==1:	
 	#compare 2 pixels of chip4
 	saveDir = "/Users/asteinhe/AstroPixData/astropixOut_tmp/injectionScans/v1/chip004/"
 	enResArr, muArr, sigmaArr, muErrArr, sigErrArr = [],[],[],[],[]
@@ -126,7 +141,7 @@ if v1:
 	for i,element in enumerate(runList):
 		enResArr_tmp, muArr_tmp, sigmaArr_tmp, nArr_tmp, muErrArr_tmp, sigErrArr_tmp = [],[],[],[],[],[]
 		for inj in injection:
-			file=f"{homeDir}/{element[0]}_amp{element[1]}/chip004_AC_{inj:.1f}Vinj_2min.h5py"
+			file=f"{homeDir}/v1/{element[0]}_amp{element[1]}/chip004_AC_{inj:.1f}Vinj_2min.h5py"
 			settings=[file, f"{inj:.1f}V injection", element[1], inj, savePlots, chip]
 			popt, enRes, pcov = enResFitting.enResPlot(settings, savedir=saveDir, integral=traceInteg, injection=True)
 			enResFitting.printParams(settings, popt, enRes, pcov, savedir=saveDir, integral=traceInteg, injection=True)
